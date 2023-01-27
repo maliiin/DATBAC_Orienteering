7792853a5ad08792130b6b2456ad1165730c91ab
maliiin <malin.astrid@lyse.net>
2023-01-27 11:46:15 +0100

    database lokalt fungerer

---
+++ orienteering/orienteering_backend/Migrations/20230127104434_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace orienteering_backend.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Track",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Track", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Checkpoint",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    QuizId = table.Column<int>(type: "int", nullable: true),
+                    GameId = table.Column<int>(type: "int", nullable: true),
+                    TrackId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checkpoint", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Checkpoint_Track_TrackId",
+                        column: x => x.TrackId,
+                        principalTable: "Track",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkpoint_TrackId",
+                table: "Checkpoint",
+                column: "TrackId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Checkpoint");
+
+            migrationBuilder.DropTable(
+                name: "Track");
+        }
+    }
+}
--- orienteering/orienteering_backend/Program.cs
+++ orienteering/orienteering_backend/Program.cs
@@ -1,11 +1,27 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using orienteering_backend.Infrastructure.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var version = new MySqlServerVersion(new Version(8, 0, 28));
+var connectionString = "server=localhost; port=3306; database=orienteering; user=root; password=passord123";
+builder.Services.AddMediatR(typeof(Program));
+
+builder.Services.AddDbContext<OrienteeringContext>(
+    options => options.UseMySql(connectionString: connectionString, serverVersion: version)
+);
 
 var app = builder.Build();
 
