dc188ad0fe2bf4f6f1ff992f60fb16a08f16b768
haraldfg <haraldfgrunn@gmail.com>
2023-01-31 15:29:06 +0100

    changed to quickchart.io qrcode api

--- orienteering/orienteering_frontend/src/pages/Home.js
+++ orienteering/orienteering_frontend/src/pages/Home.js
@@ -4,7 +4,7 @@ import QRContainer from '../components/QRContainer';
 function Home() {
     const [image, setImage] = useState("")
 
-
+    //kilder: https://devtrium.com/posts/async-functions-useeffect (31.01.2023)
     const fetchQR = async () => {
         const response = await fetch('qrcode');
         const data = await response.json();
