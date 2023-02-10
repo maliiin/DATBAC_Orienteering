9c03ac8d0196f5814a27b34b8dbc40038f4d0b20
haraldfg <haraldfgrunn@gmail.com>
2023-02-10 12:41:40 +0100

    la til noen dto's og pipelines

---
+++ orienteering/orienteering_backend/Core/Domain/Track/Dto/CheckpointNameAndQRCodeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace orienteering_backend.Core.Domain.Track.Dto
+{
+    public class CheckpointNameAndQRCodeDto
+    {
+
+        public Guid Id { get; set; }    
+        public byte[]? QRCode { get; set; }
+    }
+}
