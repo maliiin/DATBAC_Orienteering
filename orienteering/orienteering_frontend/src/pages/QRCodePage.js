548cd4c1052234efa51c0c82d0a867aeadaed67d
haraldfg <haraldfgrunn@gmail.com>
2023-02-14 10:32:28 +0100

    fjernet userid midlertidig fra qrkode logikk

--- orienteering/orienteering_frontend/src/pages/QRCodePage.js
+++ orienteering/orienteering_frontend/src/pages/QRCodePage.js
@@ -13,11 +13,11 @@ function QRCodePage() {
         const TrackId = location.state.trackid;
         //const TrackId = "08db0da6-3633-48ec-8b8f-0799ce2f9cbd";
         //const TrackId = params.TrackId;
-        const UserId = "d492968b-1d81-46f5-b2ae-eacfd20b5b5d"
+        //const UserId = "536a339d-f3f6-43f1-a921-099bdeb9fb1b";
         //const userResponse = await fetch("api/user/GetSignedInUserId").then(response => response.json());
         //const user = await userResponse.json();
        // const UserId = user.Id;
-        const url = "api/qrcode/getqrcodes?UserId=" + UserId + "&TrackId=" + TrackId;
+        const url = "api/qrcode/getqrcodes?TrackId=" + TrackId;
         //console.log("hei");
         //const data = await fetch(url).then(response => response.json());
         const response = await fetch(url);
