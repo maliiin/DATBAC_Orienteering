11dddfeeae27dcdd3d4f8925da08733e1af01b6f
haraldfg <haraldfgrunn@gmail.com>
2023-02-23 20:56:23 +0100

    added some mediator stuff

--- orienteering/orienteering_backend.Tests/UnitTest1.cs
+++ orienteering/orienteering_backend.Tests/UnitTest1.cs
@@ -4,11 +4,15 @@ using orienteering_backend.Core.Domain.Checkpoint.Pipelines;
 using orienteering_backend.Core.Domain.Checkpoint;
 using orienteering_backend.Infrastructure.Data;
 using orienteering_backend.Tests.Helpers;
+using MediatR;
 
 namespace orienteering_backend.Tests
 {
     public class UnitTest1 : InMemoryTest
     {
+
+        public IMediator mediator { get; set; }
+
         [Fact]
         public async Task CreateCheckpointTest()
         {
@@ -19,12 +23,13 @@ namespace orienteering_backend.Tests
                 _db.Database.Migrate();
             }
 
+
             var trackId = new Guid();
             Checkpoint checkpoint = new("test1", 0, trackId);
             CheckpointDto checkpointDto = new("test1", trackId, 0);
 
             var request = new CreateCheckpoint.Request(checkpointDto);
-            var handler = new CreateCheckpoint.Handler(_db, null);
+            var handler = new CreateCheckpoint.Handler(_db, mediator);
             var checkpointId = handler.Handle(request, CancellationToken.None).GetAwaiter().GetResult();
             var result = await _db.Checkpoints.FirstOrDefaultAsync(c => c.Id == checkpointId);
             Assert.Equal(checkpoint, result);
