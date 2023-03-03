3f5af620d18920ac8de58c355f0b8efb84730fd9
haraldfg <haraldfgrunn@gmail.com>
2023-03-03 14:09:15 +0100

    la til besvarelse av quiz

---
+++ orienteering/orienteering_backend/Core/Domain/Quiz/Dto/NextQuizQuestionDto.cs
@@ -0,0 +1,17 @@
+﻿namespace orienteering_backend.Core.Domain.Quiz.Dto
+{
+    public class NextQuizQuestionDto
+    {
+        public NextQuizQuestionDto(bool endOfQuiz)
+        {
+            EndOfQuiz = endOfQuiz;
+        }
+        public Guid QuizQuestionId { get; set; }
+        public string? Question { get; set; }
+        public List<Alternative>? Alternative { get; set; } = new();
+
+        public int? CorrectAlternative { get; set; }
+        public bool EndOfQuiz { get; set; }
+
+    }
+}
