f17e22b33fdab8adb057150f8045e2370df97637
haraldfg <haraldfgrunn@gmail.com>
2023-03-06 10:02:09 +0100

    la til mapperprofile

---
+++ orienteering/orienteering_backend/Infrastructure/Automapper/MapperProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using orienteering_backend.Core.Domain.Quiz;
+using orienteering_backend.Core.Domain.Quiz.Dto;
+using System.Text.RegularExpressions;
+
+// Kilder: https://github.com/hgmauri/sample-automapper/blob/main/src/Sample.Automapper.Application/MapperProfile.cs (03.03.2023)
+
+namespace orienteering_backend.Infrastructure.Automapper
+{
+
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            //Alternative
+            CreateMap<Alternative, AlternativeDto>().ReverseMap();
+
+            //Group
+            CreateMap<Group, GroupViewModel>().ReverseMap()
+                .ForMember(dest => dest.Description, src => src.Ignore())
+                .AfterMap((src, dest) =>
+                {
+                    dest.CreatedAt = DateTime.Now;
+                    dest.Description = dest.Name;
+                });
+        }
+    }
+
+}
