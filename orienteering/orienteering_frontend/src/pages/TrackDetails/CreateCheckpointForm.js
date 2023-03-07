adc0ee70aefcedd44392ee56d3e22c71ab8e73b4
maliiin <malin.astrid@lyse.net>
2023-03-07 09:04:15 +0100

    createCheckpoint, radio is cleared after submit

--- orienteering/orienteering_frontend/src/pages/TrackDetails/CreateCheckpointForm.js
+++ orienteering/orienteering_frontend/src/pages/TrackDetails/CreateCheckpointForm.js
@@ -1,15 +1,11 @@
 import { React, useState, useEffect } from "react";
-import { Link, redirect, useNavigate } from 'react-router-dom';
-import { useRadioGroup } from '@mui/material/RadioGroup';
 import { Radio, FormLabel, Box, RadioGroup, FormControlLabel, Select, MenuItem, Button, TextField } from '@mui/material';
 import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
 
-//some info of track, not details
 
 export default function CreateCheckpointForm(props) {
-    //props is props.Trackinfo (id, name, userid ...)
-    const [showForm, setShowForm] = useState(false);
+    //the chosen activity
+    const [activity, setActivity] = useState("");
 
     const [checkpointInfo, setCheckpointInfo] = useState({
         Title: "",
@@ -21,22 +17,18 @@ export default function CreateCheckpointForm(props) {
     //set userId from props
     useEffect(() => {
         setCheckpointInfo(prevState => { return { ...prevState, TrackId: props.trackId } });
-        
+
     }, [props.TrackId]);
 
 
     const handleSubmit = async (event) => {
-        //convert GameId to 0 if quiz is selected.
-
-        //it is quiz, set gameId to 0
-        
+        event.preventDefault();
         let copiedCheckpoint = JSON.parse(JSON.stringify(checkpointInfo));
 
-        if (showForm == false) {
+        if (activity == "quiz") {
+            //quiz, so gameId is 0
             copiedCheckpoint.GameId = 0;
         }
-
-        event.preventDefault();
 
         const requestOptions = {
             method: 'POST',
@@ -53,19 +45,17 @@ export default function CreateCheckpointForm(props) {
         props.updateCheckpointList();
 
         //clear text field and game
-        setCheckpointInfo({
-            ...checkpointInfo,
-            //dont update track id, its the same
-            Title: "",
-            GameId: 1
-        });
+        setCheckpointInfo(
+            {
+                ...checkpointInfo,
+                Title: "",
+                GameId: 1
+            });
 
-        //clear choises FIX clear radio
-
+        //clear radio
+        setActivity("");
 
         return response;
-
-
     };
 
     const handleChange = (event) => {
@@ -73,34 +63,15 @@ export default function CreateCheckpointForm(props) {
         setCheckpointInfo({ ...checkpointInfo, [event.target.name]: event.target.value });
     };
 
-    //fix- denne og changeGame funksjonen under kan slås sammen, de er like
-    const changeTitle = (event) => {
-        //update state
-        setCheckpointInfo({ ...checkpointInfo, [event.target.name]: event.target.value });
-    };
-
-    const changeGame = (event) => {
-        //update state
-        setCheckpointInfo({ ...checkpointInfo, [event.target.name]: event.target.value });
-    };
-
     const changeActivity = (event) => {
 
-        if (event.target.value === "spill") {
-            setShowForm(true);
-        }
-        else {
-            setShowForm(false);
-        }
+        setActivity(event.target.value);
     };
-
-
-
 
     return (
         <>
             <h4>Legg til ny post</h4>
-            
+
             <Box onSubmit={handleSubmit} component="form">
                 <TextField
                     required
@@ -117,7 +88,7 @@ export default function CreateCheckpointForm(props) {
                 <FormLabel id="radio-buttons-group">Velg aktivitet</FormLabel>
 
                 <RadioGroup
-                    
+                    value={activity}
                     aria-labelledby="radio-buttons-group"
                     name="radio-buttons-group"
                     row
@@ -128,23 +99,31 @@ export default function CreateCheckpointForm(props) {
                         value="spill"
                         label="Spill"
                         control={<Radio required={true} />}
+                        checked={activity == "spill"}
                     />
 
                     <FormControlLabel
                         value="quiz"
                         control={<Radio required={true} />}
                         label="Quiz"
+                        checked={activity == "quiz"}
                     />
 
                 </RadioGroup>
 
                 <br></br>
 
-                <FormControl required={showForm ? true :false}>
-                    <FormLabel style={showForm ? {} : { display: 'none' }} id="radio-buttons-group">Velg spill</FormLabel>
+                <FormControl //required={showForm ? true : false}
+                >
+                    <FormLabel
+                        style={activity == "spill" ? {} : { display: 'none' }}
+                        id="radio-buttons-group"
+                    >
+                        Velg spill
+                    </FormLabel>
 
                     <Select
-                        style={showForm ? {} : { display: 'none' }}
+                        style={activity == "spill" ? {} : { display: 'none' }}
                         sx={{ m: 1, minWidth: 120 }}
                         size="small"
                         labelId="select-label"
@@ -153,10 +132,7 @@ export default function CreateCheckpointForm(props) {
                         name="GameId"
                         value={checkpointInfo.GameId}
                         defaultValue={1}
-
-                        
                     >
-                        
 
                         <MenuItem value={1}>
                             Spill11
