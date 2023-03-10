8da3f4e277408a655dddf4d73eeb8ea043a6bab1
maliiin <malin.astrid@lyse.net>
2023-03-10 13:31:54 +0100

    ting faller. ikke implementert kræsj

--- orienteering/orienteering_frontend/src/pages/GamePage/FallingBoxesGame.js
+++ orienteering/orienteering_frontend/src/pages/GamePage/FallingBoxesGame.js
@@ -4,56 +4,51 @@ import { useEffect } from "react";
 
 export default function FallingBoxesGame() {
     const basketWidth = 200;
+    const basketHeight = 100;
 
-
+    const canvasWidth = window.screen.availWidth;
+    const canvasHeight = window.screen.availHeight;
 
     useEffect(() => {
+        //setup game in js
         setup(basketWidth);
     }, []);
 
-
-
-
-
-
-    
     return (<>
         <div id="container"
             style={{
                 position: "relative"
-                } }
+            }}
         >
             <canvas
                 id="gameCanvas"
                 style={{
-                    position:"absolute",
-                    width: "200px",
-                    height: "200px",
+                    position: "absolute",
+                    width: canvasWidth+"px",
+                    height: canvasHeight+"px",
                     backgroundColor: "green"
                 }}
             ></canvas>
+
             <div
                 id="basket"
                 className="draggable"
                 style={{
                     position: "absolute",
+
                     width: basketWidth + "px",
+                    height: basketHeight + "px",
+
                     backgroundColor: "lightblue",
-                    //bottom: "10px",
-                    top:"50px",
-                    height: "50px",
-
+                    top: canvasHeight-basketHeight,
                 }}
-
-
-
             >
-                Draggable Element
+         
             </div>
 
         </div>
 
 
-     
+
     </>);
 }
--- orienteering/orienteering_frontend/src/pages/GamePage/FallingBoxesScript.js
+++ orienteering/orienteering_frontend/src/pages/GamePage/FallingBoxesScript.js
@@ -1,116 +1,34 @@
 "use strict";
+var interact = require('interactjs')
 
-
-const fallingWidth = 30;
-const fallingHeigth = 30;
-const speed = 5;
+//global variables
+var fallingWidth = 30;
+var fallingHeigth = 30;
+const speed = 3;
 
 //this is position of basket
 const position = { x: 0, y: 0 }
-var interact = require('interactjs')
 var gameCanvas, BasketWidth
 var fallingObjects = [];
-
 var gameArea=null
 
 function setup(basketWidth) {
-    //gameCanvas = document.getElementById("gameCanvas");
     BasketWidth = basketWidth;
 
     gameCanvas = document.getElementById("gameCanvas");
 
     //create gameArea
     gameArea = new GameArea(gameCanvas);
+
     //start game/initialize
     gameArea.start();
 
     //make basket able to move
     moveBasket();
-
-    addFallingBox();
-
-
-    console.log(gameCanvas)
-    console.log(document.getElementById("basket"));
-
-
-    //dette skal være med
-    //window.setInterval(addFallingBox, 2000);
+    window.setInterval(addFallingBox, 2000);
 }
 
-//window.onload = loadfunk();
-
-//function loadfunk() {
-//    let rootDiv = document.getElementById("root");
-//    console.log(document.getElementById("gameCanvas"));
-//    console.log(document.getElementById("basket"));
-
-//    //console.log(rootDiv)
-//    //rootDiv.appendChild(document.createElement("div"));
-
-//    //let basket = document.createElement("div")
-//    //basket.id = "basket"
-//    //basket.className = "draggable"
-//    //basket.style.position = "absolute";
-//    //basket.style.width = 200 + "px";
-//    //basket.style.backgroundColor = "lightblue";
-//    //basket.style.bottom = "200px"
-//    //basket.style.height = "50px"
-
-//    //let basket=document.getElementById("basket")
-//    //console.log(basket)
-//    //rootDiv.appendChild(basket);
-//    //console.log(rootDiv.childElementCount);
-
-
-//    //rootDiv.reload(false)
-
-
-//}
-
-
-
-
 //https://www.w3schools.com/graphics/tryit.asp?filename=trygame_default_gravity
-//var GameArea = {
-//    canvas: gameCanvas,
-//    //canvas: document.createElement("canvas"),
-//    start: function () {
-//        //fix, flytt noe av koden her til setup
-
-//        //setup canvas
-//        this.canvas.width = window.screen.availWidth - 50
-//        this.canvas.height = window.screen.availHeight - 500
-//        this.context = this.canvas.getContext("2d");
-
-//        //this.canvas.style.position = "absolute"
-
-//        //dont let whole screen scroll
-//        let rootDiv = document.getElementById("root");
-//        rootDiv.style.width = (window.screen.availWidth - 50) + "px"
-//        rootDiv.style.touchAction = "none"
-//        let body1 = document.getElementsByTagName("body")[0];
-//        body1.style.touchAction = "none"
-//        //rootDiv.insertBefore(this.canvas, rootDiv.firstChild)
-//        //rootDiv.appendChild(document.createElement("div"))
-//        //rootDiv.reload(false)
-//        //rootDiv.insertBefore(this.canvas, rootDiv.firstChild)
-//        //rootDiv.appendChild(this.canvas)
-
-//        //rootDiv.style.position = "relative"
-//        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-//        //document.body.style.position="relative"
-//        this.interval = setInterval(updateGameArea, 20);
-
-//    },
-//    clear: function () {
-//        //clear whole canvas
-//        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-//        this.context.fillStyle = "pink"
-//        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
-//    }
-//};
-
 function GameArea(canvas) {
     this.canvas = canvas;
     //canvas: document.createElement("canvas"),
@@ -118,39 +36,31 @@ function GameArea(canvas) {
         //fix, flytt noe av koden her til setup
 
         //setup canvas
-        this.canvas.width = window.screen.availWidth - 50
-        this.canvas.height = window.screen.availHeight - 500
+        //dette skjer i react nå
+        //this.canvas.width = window.screen.availWidth - 50
+        //this.canvas.height = window.screen.availHeight - 500
         this.context = this.canvas.getContext("2d");
 
-        //this.canvas.style.position = "absolute"
 
-        //dont let whole screen scroll
+        //dont let screen scroll when dragging
+        //fix, kun body? trengs tror jeg
         let rootDiv = document.getElementById("root");
-        rootDiv.style.width = (window.screen.availWidth - 50) + "px"
         rootDiv.style.touchAction = "none"
         let body1 = document.getElementsByTagName("body")[0];
         body1.style.touchAction = "none"
-        //rootDiv.insertBefore(this.canvas, rootDiv.firstChild)
-        //rootDiv.appendChild(document.createElement("div"))
-        //rootDiv.reload(false)
-        //rootDiv.insertBefore(this.canvas, rootDiv.firstChild)
-        //rootDiv.appendChild(this.canvas)
 
-        //rootDiv.style.position = "relative"
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        //document.body.style.position="relative"
         this.interval = setInterval(updateGameArea, 20);
 
     };
     this.clear = function () {
         //clear whole canvas
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        //fix, trengs clear hvis vi har fill bg
+        //bacground of canvas
         this.context.fillStyle = "pink"
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
     };
 }
-
-
 
 function FallingObject(x, y) {
     this.pos_x = x;
@@ -166,12 +76,16 @@ function FallingObject(x, y) {
     };
 
     this.drawElement = function () {
+        console.log(this.height)
         var ctx = gameArea.context;
         ctx.fillStyle = this.color;
         ctx.fillRect(this.pos_x, this.pos_y, this.width, this.height);
     };
 
     this.onGround = function () {
+        console.log(gameArea)
+        console.log(gameArea.canvas)
+        console.log(gameArea.canvas.height)
         var bottom = gameArea.canvas.height - this.height;
         if (this.pos_y >= bottom) {
             console.log("hit bottom")
@@ -182,12 +96,10 @@ function FallingObject(x, y) {
 
     //check if fallingObject hits the basket
     this.inBasket = function () {
-
     }
 }
 
 function updateGameArea() {
-    //console.log(gameArea)
     gameArea.clear()
 
     //display all falling elements
@@ -212,12 +124,10 @@ function updateGameArea() {
 
 //this function makes basket able to move
 function moveBasket() {
-    //controllPosition();
     //kilde https://interactjs.io/docs/draggable/ 09/03.23
     interact('.draggable').draggable({
 
         //gir posisjonen
-        //onmove: () => { console.log(position.x) },
         listeners: {
             start(event) {
                 console.log("start   lll")
@@ -231,6 +141,7 @@ function moveBasket() {
             },
         },
         modifiers: [
+            //cannot be dragged outside of parent
             interact.modifiers.restrictRect({
                 restriction: 'parent'
             })
@@ -239,9 +150,11 @@ function moveBasket() {
 }
 
 function addFallingBox() {
+    console.log("added falling")
     let max = gameArea.canvas.width - fallingWidth;
     let x = Math.floor(Math.random() * max);
     let fallingBox = new FallingObject(x, 0);
+    console.log(fallingBox);
     fallingObjects.push(fallingBox);
 }
 
