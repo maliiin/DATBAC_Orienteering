561662bef9112166a4fd9575be1ab1582eff3a9a
maliiin <malin.astrid@lyse.net>
2023-03-16 13:47:04 +0100

    navigation opprettes når checkpoint lages

--- orienteering/orienteering_backend/Core/Domain/Navigation/Pipelines/CreateNavigation.cs
+++ orienteering/orienteering_backend/Core/Domain/Navigation/Pipelines/CreateNavigation.cs
@@ -1,34 +1,43 @@
-﻿//using AutoMapper;
-//using MediatR;
-//using orienteering_backend.Core.Domain.Track.Dto;
-//using orienteering_backend.Infrastructure.Data;
+﻿using AutoMapper;
+using MediatR;
+using orienteering_backend.Core.Domain.Track.Dto;
+using orienteering_backend.Infrastructure.Data;
 
-//namespace orienteering_backend.Core.Domain.Navigation.Pipelines
-//{
-//    public class CreateNavigation
-//    {
+namespace orienteering_backend.Core.Domain.Navigation.Pipelines
+{
+    public class CreateNavigation
+    {
 
-//        public record Request(
-//            string path,Guid checkpointId) : IRequest<Guid>;
+        public record Request(
+            string path, Guid checkpointId) : IRequest<Guid>;
 
 
-//        public class Handler : IRequestHandler<Request, Guid>
-//        {
-//            private readonly OrienteeringContext _db;
+        //fix ikke i bruk?
 
-//            private readonly IMapper _mapper;
+        public class Handler : IRequestHandler<Request, Guid>
+        {
+            private readonly OrienteeringContext _db;
 
-//            public Handler(OrienteeringContext db, IMapper mapper)
-//            {
-//                _db = db ?? throw new ArgumentNullException(nameof(db));
-//                _mapper = mapper ?? throw new ArgumentNullException(nameof(_mapper));
+            private readonly IMapper _mapper;
 
-//            }
+            public Handler(OrienteeringContext db, IMapper mapper)
+            {
+                _db = db ?? throw new ArgumentNullException(nameof(db));
+                _mapper = mapper ?? throw new ArgumentNullException(nameof(_mapper));
 
-//            public async Task<Guid> Handle(Request request, CancellationToken cancellationToken)
-//            {
-                
-//            }
-//        }
+            }
 
-//    }
+            public async Task<Guid> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var navigation = new Navigation();
+                navigation.ToCheckpoint = request.checkpointId;
+
+                await _db.Navigation.AddAsync(navigation, cancellationToken);
+                await _db.SaveChangesAsync(cancellationToken);
+                return navigation.Id;
+
+            }
+        }
+
+    }
+}
