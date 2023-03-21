70fcf7eaa78c4cf43998dea3c6bf1bcb9fbeed8f
haraldfg <haraldfgrunn@gmail.com>
2023-03-21 20:45:09 +0100

    la til sessioncontroller og trackLogging

---
+++ orienteering/orienteering_backend/Controllers/SessionController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MediatR;
+using orienteering_backend.Core.Domain.Track.Dto;
+using orienteering_backend.Core.Domain.Checkpoint.Pipelines;
+using System.Web.Helpers;
+
+namespace orienteering_backend.Controllers;
+
+[ApiController]
+[Route("api/session")]
+public class SessionController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public SessionController(IMediator Mediator)
+    {
+        _mediator = Mediator;
+    }
+
+    [HttpGet("setStartCheckpoint")]
+    public string setStartCheckpoint(string CheckpointId)
+    {
+        if (HttpContext.Session.GetString("StartCheckpoint") == null)
+        {
+            HttpContext.Session.SetString("StartCheckpoint", CheckpointId);
+            HttpContext.Session.SetString("StartTime", DateTime.Now.ToString());
+            Console.WriteLine(HttpContext.Session.GetString("StartCheckpoint"));
+        }
+       
+        return CheckpointId;
+    }
+
+   
+   
+
+    [HttpGet("checkTrackFinished")]
+    public TrackLoggingDto getStartCheckpoint(string toCheckpoint)
+    {
+        if (HttpContext.Session.GetString("StartCheckpoint") == null)
+        {
+            throw new Exception("SessionVariable not set");
+        }
+        else
+        {
+            var startCheckpoint = HttpContext.Session.GetString("StartCheckpoint");
+            if (startCheckpoint == toCheckpoint)
+            {
+                var trackLoggingDto = new TrackLoggingDto();
+                trackLoggingDto.StartCheckpointId = new Guid(startCheckpoint);
+                var startTimeString = HttpContext.Session.GetString("StartTime");
+                var startTime = DateTime.Parse(startTimeString);
+                trackLoggingDto.TimeUsed = startTime;
+                return trackLoggingDto;
+            }
+            else
+            {
+                var trackLoggingDto = new TrackLoggingDto();
+                trackLoggingDto.StartCheckpointId = new Guid(startCheckpoint);
+                return trackLoggingDto;
+            }
+            
+            
+            
+        }
+
+        
+    }
+}
--- orienteering/orienteering_frontend/src/pages/CheckpointNavigation/CheckpointNavigation.js
+++ orienteering/orienteering_frontend/src/pages/CheckpointNavigation/CheckpointNavigation.js
@@ -7,6 +7,7 @@ import DisplayImagesUser from './Components/DisplayImagesUser';
 export default function CheckpointNavigation() {
     //kan være greit å få inn checkpointid slik at det senere blir mulig å registrere at noen har vært på checkpointet, lagre score osv...
     const [current, setCurrent] = useState(0);
+    const [trackFinished, setTrackFinished] = useState(false);
 
     const nextImage = () => {
         setCurrent(current + 1)
@@ -29,6 +30,16 @@ export default function CheckpointNavigation() {
         if (res.ok) {
             //fix-dersom endre rekkefølge på objekter, enten endre rekkefølge her eller display med rett order
             const nav = await res.json();
+            const sessionRes = await fetch("/api/session/checkTrackFinished?toCheckpoint=" + nav.toCheckpoint);
+            if (sessionRes.ok) {
+                if (sessionRes.json.TimeUsed == null) {
+                    setTrackFinished(true);
+                }
+            }
+            else {
+                //naviger til errorside
+            }
+
             setImagesList(nav.images.map((imageInfo, index) =>
                 <>
                     <DisplayImagesUser
@@ -45,29 +56,33 @@ export default function CheckpointNavigation() {
         //fix-naviger til errorside hvis ikke?
     }
 
+    function checkFinished() {
+
+    }
+
     useEffect(() => {
-        console.log(33)
+        checkFinished();
         getNavigation();
     }, []);
 
     //<DisplayImagesUser imageInfo={imagesList[current]}></DisplayImagesUser>
-
-    return (<>
-
-   
-                {imagesList[current]}
+    if (!trackFinished) {
+        return (<>
 
 
+            {imagesList[current]}
 
 
-        <Grid container spacing={2}
-            justifyContent="space-around"
-            style={{
-                bottom: "10px",
-                position: "absolute"
-            }}
 
-        >
+
+            <Grid container spacing={2}
+                justifyContent="space-around"
+                style={{
+                    bottom: "10px",
+                    position: "absolute"
+                }}
+
+            >
                 <Grid item xs={5}>
                     <Button
                         onClick={prevImage}
@@ -91,11 +106,21 @@ export default function CheckpointNavigation() {
                     </Button>
                 </Grid>
 
-        </Grid>
-        <br></br>
+            </Grid>
+            <br></br>
 
 
-    </>
-    );
+        </>
+        );
+    }
+
+    if (trackFinished) {
+        return (
+            <>
+                <p>Track finished</p>
+            </>
+            );
+    }
+    
 
 }
