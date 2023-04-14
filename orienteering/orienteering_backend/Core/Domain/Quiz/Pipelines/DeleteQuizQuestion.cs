be5a626b5308fb6e3106188a55cadab8eb8733ed
haraldfg <haraldfgrunn@gmail.com>
2023-04-14 11:25:49 +0200

    fjernet feil navngivning på variabel

--- orienteering/orienteering_backend/Core/Domain/Quiz/Pipelines/DeleteQuizQuestion.cs
+++ orienteering/orienteering_backend/Core/Domain/Quiz/Pipelines/DeleteQuizQuestion.cs
@@ -33,11 +33,11 @@ public static class DeleteQuizQuestion
             if (userId == null) { throw new AuthenticationException("user not signed in"); }
 
             
-            var quiz2 = await _db.Quiz.Include(a => a.QuizQuestions).FirstOrDefaultAsync(q => q.QuizQuestions.Any(qq => qq.Id == request.quizQuestionId));
+            var quiz = await _db.Quiz.Include(a => a.QuizQuestions).FirstOrDefaultAsync(q => q.QuizQuestions.Any(qq => qq.Id == request.quizQuestionId));
             //check if there exists a quiz in the database containing a quizquestion with the given Id
-            if (quiz2 == null) { throw new NullReferenceException("the quiz dont exist or not allowed to access"); }
+            if (quiz == null) { throw new NullReferenceException("the quiz dont exist or not allowed to access"); }
             //check that user is allowed to access this quiz
-            var trackUser = await _mediator.Send(new GetTrackUserByQuiz.Request(quiz2.Id));
+            var trackUser = await _mediator.Send(new GetTrackUserByQuiz.Request(quiz.Id));
             if (trackUser.UserId != userId)
             {
                 //the user that owns the track is not the one signed in
@@ -45,7 +45,7 @@ public static class DeleteQuizQuestion
             }
            
 
-            quiz2.RemoveQuizQuestion(request.quizQuestionId);
+            quiz.RemoveQuizQuestion(request.quizQuestionId);
             await _db.SaveChangesAsync(cancellationToken);
 
             return true;
